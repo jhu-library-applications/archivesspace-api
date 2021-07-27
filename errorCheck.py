1c220f3342b4876728162c823be74d96d297d00e
michelle.janowiecki@jhu.edu <michelle.janowiecki@jhu.edu>
2021-07-27 12:06:19 -0400

    remove verification

---
+++ errorCheck.py
@@ -0,0 +1,44 @@
+import json
+import requests
+import secrets
+import time
+
+secretsVersion = input('To edit production server, enter the name of the secrets file: ')
+if secretsVersion != '':
+    try:
+        secrets = __import__(secretsVersion)
+        print('Editing Production')
+    except ImportError:
+        print('Editing Development')
+else:
+    print('Editing Development')
+
+startTime = time.time()
+
+baseURL = secrets.baseURL
+user = secrets.user
+password = secrets.password
+repository = secrets.repository
+
+auth = requests.post(baseURL + '/users/'+user+'/login?password='+password, verify=False).json()
+session = auth["session"]
+headers = {'X-ArchivesSpace-Session': session, 'Content_Type': 'application/json'}
+print('authenticated')
+repository =['5']
+for repo in repository:
+    endpoint = '/repositories/'+repo+'/resources'
+    ids = requests.get(baseURL+endpoint, headers=headers, verify=False).json()
+    print(ids)
+    # for id in ids:
+    #     id = str(id)
+    #     endpoint = '/repositories/'+repo+'/resources/'+id
+    #     output = requests.get(baseURL+endpoint, headers=headers, verify=False).json()
+    #     f = open(id+'_resources.json', 'w')
+    #     results = (json.dump(output, f))
+    #     f.close()
+
+
+elapsedTime = time.time() - startTime
+m, s = divmod(elapsedTime, 60)
+h, m = divmod(m, 60)
+print('Total script run time: ', '%d:%02d:%02d' % (h, m, s))
--- get/getSingleResource.py
+++ get/getSingleResource.py
@@ -1,8 +1,8 @@
 import json
 import requests
-import secrets
 import argparse
 import urllib3
+import secrets
 
 secretsVersion = input('To edit production server, enter secrets file name: ')
 if secretsVersion != '':
@@ -25,20 +25,19 @@ if args.uri:
 else:
     uri = input('Enter handle (\'/repositories/3/resources/855\'): ')
 
-uri = '/repositories/3/archival_objects/215717'
+uri = '/agents/people/1533'
 baseURL = secrets.baseURL
 user = secrets.user
 password = secrets.password
 repository = secrets.repository
-verify = secrets.verify
 
-auth = requests.post(baseURL+'/users/'+user+'/login?password='+password, verify=verify).json()
+auth = requests.post(baseURL+'/users/'+user+'/login?password='+password).json()
 session = auth['session']
 print('Session: '+session)
 headers = {'X-ArchivesSpace-Session': session, 'Content_Type': 'application/json'}
 
 print(baseURL+uri)
-output = requests.get(baseURL+uri, headers=headers, verify=verify).json()
+output = requests.get(baseURL+uri, headers=headers).json()
 uri = uri.replace('/repositories/'+repository+'/', '').replace('/', '-')
 f = open(uri+'.json', 'w')
 results = (json.dump(output, f))

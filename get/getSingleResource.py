5fd9d2c9f20e7f7cc703d73f9eff08df82a85301
Michelle Janowiecki <32551917+mjanowiecki@users.noreply.github.com>
2022-07-18 10:49:47 -0400

    Update getSingleResource.py

--- get/getSingleResource.py
+++ get/getSingleResource.py
@@ -25,20 +25,32 @@ if args.uri:
 else:
     uri = input('Enter handle (\'/repositories/3/resources/855\'): ')
 
-uri = '/agents/people/1533'
+uri = '/repositories/3/resources/1534'
 baseURL = secrets.baseURL
 user = secrets.user
 password = secrets.password
 repository = secrets.repository
 
+<<<<<<< Updated upstream
 auth = requests.post(baseURL + '/users/' + user + '/login?password=' + password).json()
+=======
+auth = requests.post(baseURL+'/users/'+user+'/login?password='+password, verify=False).json()
+print(auth)
+>>>>>>> Stashed changes
 session = auth['session']
 print('Session: ' + session)
 headers = {'X-ArchivesSpace-Session': session, 'Content_Type': 'application/json'}
 
+<<<<<<< Updated upstream
 print(baseURL + uri)
 output = requests.get(baseURL + uri, headers=headers).json()
 uri = uri.replace('/repositories/' + repository + '/', '').replace('/', '-')
 f = open(uri + '.json', 'w')
+=======
+print(baseURL+uri)
+output = requests.get(baseURL+uri, headers=headers, verify=False).json()
+uri = uri.replace('/repositories/'+repository+'/', '').replace('/', '-')
+f = open(uri+'.json', 'w')
+>>>>>>> Stashed changes
 results = (json.dump(output, f))
 f.close()
